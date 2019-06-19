1c72a84452ee4279d9c09324d9eec976e4db3df7
Denys Ordynskiy <denysord88@gmail.com>
2019-06-19 19:24:45 +0300

    Update script start_selenoid.sh to work with Chrome browser

--- start_selenoid.sh
+++ start_selenoid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/mapr/drillAutomation/selenoid/" -e OVERRIDE_HOME="/home/mapr/drillAutomation/selenoid/" aerokube/cm:latest-release selenoid start --vnc --tmpfs 2000 --last-versions 1 --browsers firefox
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/mapr/drillAutomation/selenoid/" -e OVERRIDE_HOME="/home/mapr/drillAutomation/selenoid/" aerokube/cm:latest-release selenoid start --vnc --tmpfs 2000 --last-versions 1 --browsers "firefox;chrome"
 docker run -d --name selenoid-ui -p 8090:8090 aerokube/selenoid-ui --listen :8090 --selenoid-uri http://10.234.238.57:4444
